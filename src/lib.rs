227bcccb9598428ad0c8b3fa645c43e2554ed8d2
David Rusu <davidrusu.me@gmail.com>
2020-12-29 16:35:11 +0400

    remove onboarding from the main algorithm; cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // #![deny(missing_docs)]
 
 pub mod brb_membership;
-pub use crate::brb_membership::{State, Vote, Reconfig, Ballot, VoteMsg, Generation, Error}; 
+pub use crate::brb_membership::{Ballot, Error, Generation, Reconfig, State, Vote, VoteMsg};
 
 pub mod actor;
 pub use actor::{Actor, Sig, SigningActor};
